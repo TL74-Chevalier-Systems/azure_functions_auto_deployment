a3fda0f7fadf1f73b71003cc085b56ba78faefe3
daijun-wang <domi.daijunwang@gmail.com>
2025-03-27 17:20:29 -0700

    Added testing triggers for FHA

--- TL74Functions/FinancialHealth/__init__.py
+++ TL74Functions/FinancialHealth/__init__.py
@@ -1,24 +1,12 @@
 import logging
 
 from azure.functions import HttpRequest, HttpResponse
+from .fha import process_fha
 
 
 def main(req: HttpRequest) -> HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("HTTP trigger function processed a request.")
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    response_message, status_code = process_fha(req)
 
-    if name:
-        return HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
-        )
+    return HttpResponse(response_message, status_code=status_code)
