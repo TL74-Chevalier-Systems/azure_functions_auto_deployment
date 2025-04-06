744818607090b47dff9072ac74fe73bba352620e
Amranax <amrana@student.ubc.ca>
2025-04-06 15:01:46 -0700

    Initial folder commit

---
+++ TL74Functions/LLMAnalysis/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+from azure.functions import HttpRequest, HttpResponse
+from .helpers import process_filing_request
+
+
+def main(req: HttpRequest) -> HttpResponse:
+    logging.info("HTTP trigger function processed a request.")
+
+    response_message, status_code = process_filing_request(req)
+
+    return HttpResponse(response_message, status_code=status_code)
