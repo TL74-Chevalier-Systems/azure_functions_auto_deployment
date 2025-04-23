66e895732b319c488e1ca08ecc3ba18bfe044e8e
Amranax <amrana@student.ubc.ca>
2025-04-22 20:18:53 -0700

    Update to fix submodules

--- TL74Functions/TestingDeployment/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from azure.functions import HttpRequest, HttpResponse
-
-
-def main(req: HttpRequest) -> HttpResponse:
-    logging.info("Testing deploument trigger.")
-
-    return HttpResponse("Testing Deployment", status_code=200)
